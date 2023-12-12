18b0010c1af90ea590d5a7187229cc195ad11982
anilmekala1768 <52775097+anilmekala1768@users.noreply.github.com>
2023-12-12 09:07:39 +0530

    Update Number_of_islands.java

--- Graphs/Number_of_islands.java
+++ Graphs/Number_of_islands.java
@@ -1,3 +1,6 @@
+/* https://practice.geeksforgeeks.org/problems/find-the-number-of-islands/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find_the_number_of_islands
+*/
+
 class Pair{
     int first;
     int second;
